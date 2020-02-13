c2e733216aa62b246e7a9afbc81d6c7cc1918661
Andrew Ball <andy.bob.ball@gmail.com>
2020-02-13 09:23:59 -0600

    arrays

---
+++ js/bonus-arrays.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+var fruits = ["apple", "banana", "orange", "apple", "pineapple"];
+
+
+function allIndexesOf(inputA, inputB) {
+    console.log(inputA.indexOf(inputB), inputA[array.length - 1])
+}
+allIndexesOf(fruits, "apple");
+
+
+// allIndexesOf(fruits, "guava");
+// allIndexesOf(fruits, "pineapple");
+
+// console.log(removeAll(fruits, "apple"));
+
