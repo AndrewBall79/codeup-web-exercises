36cbfa2a0353c395cd6c954222aa6c9bb049561b
Andrew Ball <andy.bob.ball@gmail.com>
2020-02-16 11:15:06 -0600

    misplaced planets push

---
+++ js/planets-string.js
@@ -0,0 +1,20 @@
+(function(){
+    "use strict";
+
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray; 
+
+    // TODO: Convert planetsString to an array, save it to planetsArray.
+    planetsString.split('|');
+    console.log(planetsArray);
+
+    // TODO: Create a string with <br> tags between each planet. console.log() your results.
+    //       Why might this be useful?
+    var planetsString = "Mercury<br>Venus<br>Earth<br>Mars<br>Jupiter<br>Saturn<br>Uranus<br>Neptune";
+    var planetsArray2
+    console.log(planetsArray2);
+
+    // Bonus: Create a second string that would display your planets in an undordered list.
+    //        You will need an opening AND closing <ul> tags around the entire string, and <li> tags around each planet.
+    //        console.log() your results.
+})();
