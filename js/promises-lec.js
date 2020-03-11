92a01940f06ed25f2f28512ef086a7410e4b8cfe
Andrew Ball <andy.bob.ball@gmail.com>
2020-03-11 17:01:05 -0500

    Wednesday

---
+++ js/promises-lec.js
@@ -0,0 +1,95 @@
+'use strict';
+// once the promise is resolved or rejected, take additional action
+let goodKid = false;
+
+const getCake = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (goodKid) {
+            resolve("CAAAAAAKE!!");
+        } else {
+            reject("NO CAAAAKE!!! :((((");
+        }
+    }, 2000);
+});
+
+
+// once the promise is resolved or rejected, take additional action
+
+getCake.then((data) => {
+    console.log(data);
+});
+
+getCake.catch((error) => {
+    console.log(error);
+    console.log(error);
+}).then(() => {
+    console.log("Additional then");
+    return Promise.reject();
+}).catch(() => {
+    console.log("Final catch");
+});
+
+
+// ============ Using Promises with jQuery version 3+
+// 50  promises/promises-song.html
+// @@ -21,7 +21,7 @@ <h3 id="song"></h3>
+
+// A Song from Callback Hell...
+
+
+//
+// function singLyrics(lyrics, pitch, callback) {
+//     var lyric = new SpeechSynthesisUtterance(lyrics);
+//     lyric.pitch = pitch;
+// @@ -59,30 +59,30 @@ <h3 id="song"></h3>
+
+
+// A Simpler Song to Read (I promise)... but still sung terribly...
+//
+// function singLyrics(lyrics, pitch) {
+//     return new Promise((res) => {
+//         var lyric = new SpeechSynthesisUtterance(lyrics);
+//         lyric.pitch = pitch;
+//         lyric.rate = .7;
+//         speechSynthesis.speak(lyric);
+//         song.append(lyrics);
+//         setTimeout(res, 1500);
+//     });
+// }
+//
+// const singPromises = () => {
+//     return singLyrics('All a-', .5)
+//         .then(() => singLyrics('round the', 1))
+//         .then(() => singLyrics(' mulberry', 1.5))
+//         .then(() => singLyrics(' bush', 1))
+//         .then(() => singLyrics(' the monkey', .5))
+//         .then(() => singLyrics(' chased', 1))
+//         .then(() => singLyrics(' the wea-', 1.5))
+//         .then(() => singLyrics(' sel', 1));
+// };
+//
+// document.getElementById('sing-btn').addEventListener('click', singPromises);
+
+function singLyrics(lyrics, pitch) {
+    return new Promise((res) => {
+        var lyric = new SpeechSynthesisUtterance(lyrics);
+        lyric.pitch = pitch;
+        lyric.rate = .7;
+        speechSynthesis.speak(lyric);
+        song.append(lyrics);
+        setTimeout(res, 1500);
+    });
+}
+
+const singPromises = () => {
+    return singLyrics('All a-', .5)
+        .then(() => singLyrics('round the', 1))
+        .then(() => singLyrics(' mulberry', 1.5))
+        .then(() => singLyrics(' bush', 1))
+        .then(() => singLyrics(' the monkey', .5))
+        .then(() => singLyrics(' chased', 1))
+        .then(() => singLyrics(' the wea-', 1.5))
+        .then(() => singLyrics(' sel', 1));
+};
+
+document.getElementById('sing-btn').addEventListener('click', singPromises);
---
+++ js/promises.js
@@ -0,0 +1,134 @@
+'use strict';
+
+
+// let goodKid = true;
+// const getCake = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if (goodKid === true) {
+//             resolve('CAKE!');
+//         } else {
+//             reject('No Cake!')
+//         }
+//
+//     });
+// });
+// getCake.then((data) => {
+//     console.log(data);
+// });
+//
+// getCake.catch((error) => {
+//     console.log(error)
+// });
+
+// const wait = num => {
+//     return new Promise((res) => {
+//         setTimeout(res, num);
+//         });
+//     };
+//
+// wait(1000).then(() => console.log("You\'ll see this after 1 second"));
+// wait(3000).then(() => console.log("You\'ll see this after 3 seconds"));
+//
+//
+//
+// Promise.resolve('one').then((one) => {
+//     console.log(one);
+//     return 'two';
+// }).then((two) => {
+//     console.log(two);
+//     return 'three';
+// }).then((three) => {
+//     console.log(three);
+// });
+
+
+// $.ajax('https://swapi.co/api/people/1');
+
+// fetch('https://swapi.co/api/people').then(response => {
+//     return response.json();
+// }).then(data => {
+//     console.log(data)
+// }).catch(error => {
+//     console.log(error)
+// });
+
+
+//CRUD Create Read Update Display
+
+// fetch(''), {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         userName: 'bill',
+//         password: '1234'
+//     })
+// };
+
+//Create a function that accepts a GitHub username, and returns a promise that resolves with the date of the last commit that user made
+//To find the most recent commit, find the most recent public push event payload and use the first commit in the payload to find it's date.
+//
+
+fetch('https://api.github.com/users/AndrewBall79', {headers: {'Authorization': 'token b48c101eb866a02cdef2f9763282b3e9564e3578'}})
+    .then((response) => {
+        return response.json()
+            .then((users) => {
+                var i = users.updated_at;
+                console.log(i);
+            });
+    });
+
+
+// =================== Fetch Bonuses
+//
+// -- Ex. 1
+//
+// In your data directory, create a profiles.json file with the following...
+//
+// {
+//     "sandy123": "brown",
+//     "bob234": "green",
+//     "sally345": "blue"
+// }
+//
+// Add an input field and submit button (you will need to prevent the default behaviour if inside a form element) that when clicked, runs the following function...
+// Create a function that takes in a username and fetch's the color preference based on the username and sets the background color to that color.
+//
+
+
+// -- Ex. 2
+//
+// Using the Star Wars API, log the first film a given Star Wars character's homeworld appeared in
+//
+//
+//
+// -- Ex. 3
+//
+// Using the GitHub API and reduce(), find the average hour of the last 3 pushes. Ignore minutes.
+//
+//
+//
+// -- Ex. 4
+//
+// Create star-chars.json file in your data folder and paste in the following...
+//
+// [
+//     {
+//         "name": "Rey",
+//         "coolness": 9
+//     },
+//     {
+//         "name": "Luke Skywalker",
+//         "coolness": 9
+//     },
+//     {
+//         "name": "Darth Vadar",
+//         "coolness": 10
+//     }
+// ]
+//
+// Create a function that fetches this array and returns an array of homeworlds for the characters.
+//     It will look something like the following...
+//
+// ['Jakku', 'Tatooine', 'Tatooine']
